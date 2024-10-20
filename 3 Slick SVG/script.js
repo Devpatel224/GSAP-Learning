e5a501c82f2226f3a49a1921ca670ffb46fb958b
Dev Patel <devpatel9773@gmail.com>
2024-10-20 08:50:09 +0530

    Learn About Slick SVG and GSAP element attribute change

---
+++ 3 Slick SVG/script.js
@@ -0,0 +1,24 @@
+var initialePath = `M 10 100 Q 500 100 990 100`
+
+var finalPath = `M 10 100 Q 500 100 990 100`
+
+var string = document.querySelector('#string')
+
+string.addEventListener("mousemove",(dets)=>{
+    initialePath = `M 10 100 Q ${dets.x} ${dets.y} 990 100`    
+    
+    gsap.to("svg path",{
+        attr:{d:initialePath},
+        duration:0.2,
+        ease:"power3.out"
+    })
+})
+
+string.addEventListener("mouseleave",(dets)=>{
+    gsap.to("svg path",{
+        attr:{d:finalPath},
+        duration:1.5,
+        ease:"elastic.out(1,0.2)"
+    })
+})
+
